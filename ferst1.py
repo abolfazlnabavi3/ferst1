38b19e18f12cd01be1061681025cd2e15d5feb0d
abolfazlnabavi3 <abolfazlnab6@gmail.com>
2025-04-13 00:37:57 +0330

    ferst

---
+++ ferst1.py
@@ -0,0 +1,31 @@
+#this is for Mathematical operations
+#abolfazl nabavi
+
+
+n1 = 8
+n2 = 12
+
+
+n3 = n1 * n2
+print(n3)
+
+
+n4 = n1 ** n2
+print(n4)
+
+n5 = n1 // n2
+print(n5)
+
+n6 = n1 / n2
+print(n6)
+
+n7 = n1 % n2
+print(n7)
+
+n8 = n1 + n2
+print(n8)
+
+n9 = n1 - n2
+print(n9)
+
+
